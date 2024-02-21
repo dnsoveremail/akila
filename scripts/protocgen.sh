071be9524b6a49291ffb774e0f81b0dda9617eba
abgr <tbpmtbpm@gmail.com>
2024-02-21 16:11:02 +0330

    fix protocgen.sh

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -12,13 +12,13 @@ for dir in $proto_dirs; do
   proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
   for file in $proto_files; do
     # Check if the go_package in the file is pointing to evmos
-    if grep -q "option go_package.*evmos" "$file"; then
+    if grep -q "option go_package.*akila" "$file"; then
       buf generate --template proto/buf.gen.gogo.yaml "$file"
     fi
   done
 done
 
 # move proto files to the right places
-cp -r github.com/evmos/evmos/v*/* ./
+cp -r github.com/akila/akila/v*/* ./
 rm -rf github.com
 
